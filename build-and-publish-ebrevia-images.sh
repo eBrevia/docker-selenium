7bb44cb656648297d5b77fe471acd7050a05a9b6
Victor Smirnov <vsmirnov@ebrevia.com>
2023-06-05 23:20:34 +0000

    Don't cache images

--- build-and-publish-ebrevia-images.sh
+++ build-and-publish-ebrevia-images.sh
@@ -13,27 +13,26 @@ VERSION=3.141.59-$DATE
 AUTHORS=victor.smirnov@dfinsolutions.com
 NAMESPACE=ebreviainc
 
-
 cd Base
-docker build -t $NAMESPACE/selenium-grid:base-$VERSION .
+docker build --pull --no-cache -t $NAMESPACE/selenium-grid:base-$VERSION .
 docker push $NAMESPACE/selenium-grid:base-$VERSION
 
 cd ../NodeBase
 . generate.sh $VERSION $NAMESPACE $AUTHORS
-docker build -t $NAMESPACE/selenium-grid:node-base-$VERSION .
+docker build --pull --no-cache -t $NAMESPACE/selenium-grid:node-base-$VERSION .
 docker push $NAMESPACE/selenium-grid:node-base-$VERSION
 
 cd ../Hub
 . generate.sh $VERSION $NAMESPACE $AUTHORS
-docker build -t $NAMESPACE/selenium-grid:hub-$VERSION .
+docker build --pull --no-cache -t $NAMESPACE/selenium-grid:hub-$VERSION .
 docker push $NAMESPACE/selenium-grid:hub-$VERSION
 
 cd ../NodeChrome
 . generate.sh $VERSION $NAMESPACE $AUTHORS
-docker build -t $NAMESPACE/selenium-grid:node-chrome-$VERSION .
+docker build --pull --no-cache -t $NAMESPACE/selenium-grid:node-chrome-$VERSION .
 docker push $NAMESPACE/selenium-grid:node-chrome-$VERSION
 
 cd ../NodeFirefox
 . generate.sh $VERSION $NAMESPACE $AUTHORS
-docker build -t $NAMESPACE/selenium-grid:node-firefox-$VERSION .
+docker build --pull --no-cache -t $NAMESPACE/selenium-grid:node-firefox-$VERSION .
 docker push $NAMESPACE/selenium-grid:node-firefox-$VERSION
